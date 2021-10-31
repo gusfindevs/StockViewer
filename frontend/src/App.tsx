41fe0c6d8ab2784ac677302d60277627c0cd745f
GusFinDevs <gusfindevs@gmail.com>
2021-10-31 19:54:03 -0300

    Introduzido recebimento de children por Content. Layout também recebe children. App tem que passar a page Dashboard pra Layout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </>
   );
 }
