7dc1224dbe862acdc848c5c1fb065904fa09e028
GusFinDevs <gusfindevs@gmail.com>
2021-10-31 19:20:35 -0300

    Adicionados stylings de Aside, MainHeader, Content e Layout

--- frontend/src/components/Layout/styles.ts
+++ frontend/src/components/Layout/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+/**
+ * MH = Main Header
+ * AS = Aside
+ * CT = Content
+ */
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 200px auto;
+  grid-template-rows: 40px auto;
+
+  grid-template-areas:
+    'MH MH'
+    'AS CT';
+
+  height: 100vh;
+`;
