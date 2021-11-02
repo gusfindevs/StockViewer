73239657e065c8b9145a15af692f3f056e4d484b
GusFinDevs <gusfindevs@gmail.com>
2021-11-02 20:13:45 -0300

    Preparando Dashboard para receber boxes.

--- frontend/src/components/Content/styles.ts
+++ frontend/src/components/Content/styles.ts
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   grid-area: CT;
 
-  background-color: #eaeded; ;
+  background-color: #eaeded;
+
+  padding: 25px;
 `;
