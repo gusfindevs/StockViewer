75daad8fed9c2a8fcc44186a964ed7082bfb9b78
GusFinDevs <gusfindevs@gmail.com>
2021-11-10 18:28:59 -0300

    Arrumando API fastapi para probleminha de CORS policy.

--- api/main.py
+++ api/main.py
@@ -5,6 +5,8 @@ import uvicorn
 
 import mysql.connector
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 mydb = mysql.connector.connect(
   host=os.environ.get('MYSQL_HOST'),
@@ -15,6 +17,19 @@ mydb = mysql.connector.connect(
 )
 
 app = fastapi.FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def index():
